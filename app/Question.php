e4268a05116b5fa78c0447a95c6e3050e0695f39
afsarpervez <afsarpervez@gmail.com>
2021-02-18 11:31:44 +0600

    add author, url and date

--- app/Question.php
+++ app/Question.php
@@ -17,6 +17,15 @@ class Question extends Model
         $this->attributes['title']=$value;
         $this->attributes['slug']=str_slug($value);
     }
+    public function getUrlAttribute()
+    {
+        //return route('questions.show', $this->id);
+        return '#';
+    }
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 
     //$question->Question::find(1);
     //$question->user->email;
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -11,7 +11,12 @@
                     @foreach($questions as $question)
                         <div class="media">
                             <div class="media-body">
-                                <a href="{{$question->slug}}"><h3 class="mt-0">{{$question->title}}</h3></a>
+                                <h3 class="mt-0"><a href="{{$question->url}}">{{$question->title}}</a></h3>
+                                <p class="lead">
+                                    Ask by
+                                    <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                                    <small class="text-muted">{{$question->created_date}}</small>
+                                </p>
                                 {{str_limit($question->body, 250)}}
                             </div>                            
                         </div>
